211bde2f3bc95b78ecedbc3c8308648496123283
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2018-12-04 01:04:38 +0300

    removed deprecation

--- src/main/scala/generators/DescriptionReader.scala
+++ src/main/scala/generators/DescriptionReader.scala
@@ -3,14 +3,14 @@ package generators
 import java.nio.file.{Files, Path, Paths}
 
 import input.FileDescription
-import org.antlr.v4.runtime.{ANTLRInputStream, CharStreams, CommonTokenStream}
+import org.antlr.v4.runtime.{CharStreams, CommonTokenStream}
 import parser.{InputLexer, InputParser}
 import utils.IOUtils
 
 class DescriptionReader(path: Path) {
   def getFileDescription(): FileDescription = {
     IOUtils.using(Files.newInputStream(path)) { stream =>
-      val charStream = new ANTLRInputStream(stream)
+      val charStream = CharStreams.fromStream(stream)
       val lexer = new InputLexer(charStream)
       val tokens = new CommonTokenStream(lexer)
       val parser = new InputParser(tokens)
