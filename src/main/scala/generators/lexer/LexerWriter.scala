a30b3454aac94311b8ebb5f197855e54f4adbfbf
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2018-12-13 00:32:01 +0300

    Added lexer writer

--- src/main/scala/generators/lexer/LexerWriter.scala
+++ src/main/scala/generators/lexer/LexerWriter.scala
@@ -5,7 +5,7 @@ import java.nio.file.{Files, Path, StandardOpenOption}
 import utils.IOUtils
 
 object LexerWriter {
-  def writeLexer(pathToFileWithLexer: Path, lexerCode: String) = {
+  def writeLexer(pathToFileWithLexer: Path, lexerCode: String) {
     IOUtils.using(Files.newBufferedWriter(pathToFileWithLexer, StandardOpenOption.CREATE)) { writer =>
       writer.write(lexerCode)
       writer.newLine()
