27df85f7c6eb6478d11d9b9355914d510a425233
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2018-12-19 00:01:32 +0300

    FINISHED

--- src/test/scala/test/ParserGeneratorTest.scala
+++ src/test/scala/test/ParserGeneratorTest.scala
@@ -1,21 +1,45 @@
 package test
 
-import java.nio.file.{Files, Paths}
+import java.nio.file.{Files, Path, Paths}
 
 import generated.{ListDescriptionLexer, ListDescriptionParser}
 import generators.Generator
-import org.scalatest.FlatSpec
+import org.scalatest.{BeforeAndAfterAll, FlatSpec}
+import java.io.IOException
+import java.nio.file.FileVisitResult
+import java.nio.file.Files
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.attribute.BasicFileAttributes
 
-class ParserGeneratorTest extends FlatSpec {
-  "ParserGenerator" should "genarate parser for list descriptions" in {
-    /*val gen = new Generator(
+class ParserGeneratorTest extends FlatSpec with BeforeAndAfterAll {
+  override def beforeAll(): Unit = {
+    Files.walkFileTree(Paths.get("src/test/scala/generated"), new ParserGeneratorTest.TempDirectoryCleaner)
+
+    val gen = new Generator(
       Paths.get("src/test/resources/ListDescription"),
       Paths.get("src/test/scala/generated")
     )
-    gen.generate()*/
+    gen.generate()
+  }
+
+  "ParserGenerator" should "parse list descriptions and describe lists" in {
     val stream = Files.newInputStream(Paths.get("src/test/resources/ListExample"))
     val lexer = new ListDescriptionLexer(stream)
     val parser = new ListDescriptionParser(lexer)
     parser.parseLIST()
+  }
+}
+
+object ParserGeneratorTest {
+  class TempDirectoryCleaner extends SimpleFileVisitor[Path] {
+    override def visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult = {
+      Files.delete(file)
+      FileVisitResult.CONTINUE
+    }
+
+    override def postVisitDirectory(dir: Path, exc: IOException): FileVisitResult = {
+      Files.delete(dir)
+      FileVisitResult.CONTINUE
+    }
   }
 }
